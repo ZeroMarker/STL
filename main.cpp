29b38eeccfb41af1d92ec5274709c85f6acc9c8b
ZeroMarker <u78k4l67@163.com>
2023-11-21 23:10:56 +0800

    Add doxygen

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include "Unordered/unordered.h"
 #include "Template/template.h"
 
+
 int main() {
+
     template_();
     sequence();
     associative();
@@ -13,4 +15,20 @@ int main() {
     // TODO write some code
     std::cout << "Hello, World!" << '\n';
     return 0;
+}
+/**
+ * Retrieve the value stored in three selected Cells
+ *
+ * @param a numeric id representing the 1st desired cell
+ *
+ * @return value stored in the Cell
+ *
+ * @pre (cell1Id > 0 && cell1Id < 10) &&
+ *      (cell2Id > 0 && cell2Id < 10) &&
+ *      (cell3Id > 0 && cell3Id < 10)
+ */
+int display(int a) {
+    // doxygen
+    std::cout << a << std::endl;
+    return a + 1;
 }
